d54bdf99033c949944bbab6c38893d3adfbedeb8
Folasade Ekulona <ekulonaf@gmail.com>
2023-03-15 19:43:55 -0400

    adds completed loop lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+
+const names = ["Guadalupe", "Ollie", "Aki"];
+const messages = [];
+
+function writeCards(names, value) {
+    for (let i = 0; i < names.length; i ++) {
+        let message = `Thank you, ${names[i]}, for the wonderful ${value} gift!`;
+        messages.push(message);
+    }
+    return messages; 
+}
+
+function countDown(integer) {
+    while (integer >= 0) {
+        console.log(integer);
+        integer --;
+    }
+}
